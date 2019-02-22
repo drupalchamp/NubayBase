a23067760f5add71092bc8b8145b2aa2b18ce068
Vikas kumar <drupalchamp@gmail.com>
2019-02-22 11:57:01 +0530

    changes in template file for new content type

--- sites/all/themes/nubay/templates/node--proof-of-concept.tpl.php
+++ sites/all/themes/nubay/templates/node--proof-of-concept.tpl.php
@@ -12,14 +12,19 @@
     $title =  $node->title;
     $field_upload_background_image_url = 'none';
     $field_specify_color_for_background = 'transparent';
-
+    $background_options = array();
+	if(!empty($node->field_background_options['und'])){
+	  foreach($node->field_background_options['und'] as $background_option){
+        $background_options [] = $background_option['value'];
+	  }
+	}
     if(!empty($node->field_text_content)){
       $field_text_content = $node->field_text_content['und'][0]['value'];
     }
-	if(!empty($node->field_specify_color_for_backgrou)){
+	if(!empty($node->field_specify_color_for_backgrou) && in_array('1',$background_options)){
       $field_specify_color_for_background = $node->field_specify_color_for_backgrou['und'][0]['rgb'];
     }
-    if(!empty($node->field_upload_background_image)){
+    if(!empty($node->field_upload_background_image)&& in_array('2',$background_options)){
       $field_upload_background_image_url = 'url('.file_create_url($node->field_upload_background_image['und'][0]['uri']).')';
     }
   ?>
